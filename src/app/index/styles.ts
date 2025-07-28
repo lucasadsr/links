8311453aa759a9c1238f545e6cf3aa686e5c2af7
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-07-28 11:50:22 -0300

    feat: add initial Index component and styles, define color palette

---
+++ src/app/index/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { colors } from "@/src/styles/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: colors.green[900],
+    fontSize: 22,
+  },
+});
