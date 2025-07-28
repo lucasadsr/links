f91c48fa3c6f0a191c6775cce5b3d2d96203655e
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-07-28 11:28:58 -0300

    Remove unused components and files, update color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,18 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+export const colors = {
+  gray: {
+    100: "#F4F4F5",
+    200: "#E4E4E7",
+    300: "#D4D4D8",
+    400: "#A1A1AA",
+    500: "#71717A",
+    600: "#52525B",
+    800: "#27272A",
+    900: "#18181B",
+    950: "#09090B",
+  },
 
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+  green: {
+    300: "#2DD4BF",
+    900: "#042F2E",
   },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
-};
+}
