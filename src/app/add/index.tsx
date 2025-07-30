1d707b7ef17848cf6d304e179ba8edb51c49d94d
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-07-30 09:48:43 -0300

    feat: enhance Add screen with input fields and validation alerts

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,12 +1,41 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { styles } from "@/styles/screens/add";
 import { router } from "expo-router";
 import { Categories } from "@/components/categories";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { useState } from "react";
 
 export default function Add() {
+  const [category, setCategory] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+
+  function handleAdd() {
+    if (!category) {
+      return Alert.alert(
+        "Selecione uma categoria",
+        "Por favor, preencha todos os campos obrigatórios."
+      );
+    }
+    if (!name.trim()) {
+      return Alert.alert(
+        "Preencha o nome",
+        "Por favor, preencha todos os campos obrigatórios."
+      );
+    }
+    if (!url.trim()) {
+      return Alert.alert(
+        "Preencha a URL",
+        "Por favor, preencha todos os campos obrigatórios."
+      );
+    }
+    console.log({ category, name, url });
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -18,8 +47,13 @@ export default function Add() {
       </View>
 
       <Text style={styles.label}>Selecione uma categoria</Text>
+      <Categories onChange={setCategory} selected={category} />
 
-      <Categories />
+      <View style={styles.form}>
+        <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
+      </View>
     </SafeAreaView>
   );
 }
